0edf9d56621a0eadacbb44016632d999ad582294
Igor Meira <meira.igor@gmail.com>
2020-05-26 19:32:50 -0300

    v1 componentes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sba7App
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ResendRegistrationTokenComponent } from './components/resend-registration-token/resend-registration-token.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HeaderComponent } from './shared/components/navigation/header/header.component';
+import { DeleteUserModalComponent } from './shared/components/modals/delete-user-modal/delete-user-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    ResendRegistrationTokenComponent,
+    EditUserComponent,
+    ListUserComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    DeleteUserModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
