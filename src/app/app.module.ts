48d143f1695474f3c216485125deeaf5e93c0c2a
Igor Meira <meira.igor@gmail.com>
2020-05-28 15:49:10 -0300

    implementando a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { ListUserComponent } from './components/list-user/list-user.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { HeaderComponent } from './shared/components/navigation/header/header.component';
 import { DeleteUserModalComponent } from './shared/components/modals/delete-user-modal/delete-user-modal.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ApiService } from './core/api.service';
 
 @NgModule({
   declarations: [
@@ -28,10 +31,12 @@ import { DeleteUserModalComponent } from './shared/components/modals/delete-user
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule, FormsModule
 
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
